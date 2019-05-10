8a81046402fb1a0de4abe1c93173d5acbd9a1e44
Yasin Dalal <yasin@dalal.ch>
2019-05-11 01:04:53 +0200

    Removed unused deprecated code.

--- src/main/java/me/devsnox/playtime/commands/PlayTimeCommand.java
+++ src/main/java/me/devsnox/playtime/commands/PlayTimeCommand.java
@@ -1,13 +1,11 @@
 package me.devsnox.playtime.commands;
 
 import me.devsnox.playtime.playtime.TimeManager;
-import me.devsnox.playtime.playtime.TimePlayer;
 import me.devsnox.playtime.utils.Messages;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 
 import java.util.concurrent.TimeUnit;
@@ -26,21 +24,6 @@ public class PlayTimeCommand implements CommandExecutor {
     }
 
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        /*
-            @Deprecated will be removed in the next version (v.0.4)
-         */
-        if (sender instanceof ConsoleCommandSender) {
-            if (args.length == 3 && args[0].equalsIgnoreCase("convert")) {
-                if(Long.valueOf(args[2]) > 0) {
-                    TimePlayer timePlayer = this.timeManager.getPlayedTime(Bukkit.getPlayer(args[1]).getUniqueId());
-                    timePlayer.setTime(Long.valueOf(args[2]) * 60 * 60 * 1000);
-
-                    this.timeManager.getPlayers().replace(timePlayer.getUuid(), timePlayer);
-
-                    return true;
-                }
-            }
-        }
 
         if(args.length == 0) {
             if(!(sender instanceof Player)) {
