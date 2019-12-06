0bee595e29aee5f34f40da3979a3be6bac66ed09
Yasin Dalal <yasin@dalal.ch>
2019-12-06 14:35:07 +0100

    Turned PlayTimePlaceholder into Kotlin class

--- src/main/java/me/devsnox/playtime/playtime/PlayTimePlaceholder.kt
+++ src/main/java/me/devsnox/playtime/playtime/PlayTimePlaceholder.kt
@@ -1,10 +1,9 @@
-package me.devsnox.playtime.playtime;
+package me.devsnox.playtime.playtime
 
-import me.clip.placeholderapi.external.EZPlaceholderHook;
-import org.bukkit.entity.Player;
-import org.bukkit.plugin.Plugin;
-
-import java.util.concurrent.TimeUnit;
+import me.clip.placeholderapi.external.EZPlaceholderHook
+import org.bukkit.entity.Player
+import org.bukkit.plugin.Plugin
+import java.util.concurrent.TimeUnit
 
 /**
  * Created by Yasin Dalal
@@ -12,22 +11,12 @@ import java.util.concurrent.TimeUnit;
  * GitHub: https://github.com/DevSnox
  * E-Mail: yasin@dalal.ch
  */
-public class PlayTimePlaceholder extends EZPlaceholderHook {
-
-    private TimeManager timeManager;
-
-    public PlayTimePlaceholder(Plugin plugin, TimeManager timeManager, String identifier) {
-        super(plugin, identifier);
-        this.timeManager = timeManager;
-    }
+class PlayTimePlaceholder(plugin: Plugin?, private val timeManager: TimeManager, identifier: String?) : EZPlaceholderHook(plugin, identifier) {
 
     //TODO: Add support for all available time-units (days, hours, minutes and seconds)
-
-    @Override
-    public String onPlaceholderRequest(Player player, String identifier) {
-        if(identifier.equalsIgnoreCase("time_hours")) {
-            return String.valueOf(TimeUnit.MILLISECONDS.toHours(this.timeManager.getPlayedTime(player.getUniqueId()).getTime()));
-        }
-        return "0";
+    override fun onPlaceholderRequest(player: Player, identifier: String): String {
+        return if (identifier.equals("time_hours", ignoreCase = true)) {
+            TimeUnit.MILLISECONDS.toHours(timeManager.getPlayedTime(player.uniqueId).time).toString()
+        } else "0"
     }
 }
