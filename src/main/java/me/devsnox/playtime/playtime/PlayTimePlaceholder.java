5c2d49f3267e5bce11bc0f0ee9a11a671efe80a0
DevSnox <me.devsnox@gmail.com>
2020-03-03 17:06:29 +0100

    Updated and fixed placeholder-api implementation.

--- src/main/java/me/devsnox/playtime/playtime/PlayTimePlaceholder.java
+++ src/main/java/me/devsnox/playtime/playtime/PlayTimePlaceholder.java
@@ -1,6 +1,6 @@
 package me.devsnox.playtime.playtime;
 
-import me.clip.placeholderapi.external.EZPlaceholderHook;
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
@@ -11,20 +11,38 @@ import java.util.concurrent.TimeUnit;
  * E-Mail: me.devsnox@gmail.com
  * Skype: DevSnox
  */
-public class PlayTimePlaceholder extends EZPlaceholderHook {
+public class PlayTimePlaceholder extends PlaceholderExpansion {
 
-    private TimeManager timeManager;
+    private final Plugin plugin;
+    private final String identifier;
+    private final TimeManager timeManager;
 
-    public PlayTimePlaceholder(Plugin plugin, TimeManager timeManager, String identifier) {
-        super(plugin, identifier);
+    public PlayTimePlaceholder(final Plugin plugin, final TimeManager timeManager, final String identifier) {
+        this.plugin = plugin;
+        this.identifier = identifier;
         this.timeManager = timeManager;
     }
 
     @Override
-    public String onPlaceholderRequest(Player player, String identifier) {
-        if(identifier.equalsIgnoreCase("time_hours")) {
+    public String onPlaceholderRequest(final Player player, final String identifier) {
+        if (identifier.equalsIgnoreCase("time_hours")) {
             return String.valueOf(TimeUnit.MILLISECONDS.toHours(this.timeManager.getPlayedTime(player.getUniqueId()).getTime()));
         }
         return "0";
+    }
+
+    @Override
+    public String getIdentifier() {
+        return this.identifier;
+    }
+
+    @Override
+    public String getAuthor() {
+        return this.plugin.getDescription().getAuthors().get(0);
+    }
+
+    @Override
+    public String getVersion() {
+        return this.plugin.getDescription().getVersion();
     }
 }
